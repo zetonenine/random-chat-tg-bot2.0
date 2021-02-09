04c3af1d08b5548be18035961b87586fad79be9c
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-02-09 15:42:44 +0300

    Put redis_file

---
+++ redis_file.py
@@ -0,0 +1,22 @@
+import redis
+
+
+r = redis.Redis(db=0)
+
+
+def redis_add_new_user(chatID):
+    r.hset(0, chatID, "")
+
+
+def redis_add_order(chatID):
+
+    r.zadd("mylist", {chatID: 0})
+    return r.zrange("mylist", 0, -1, withscores=True)
+
+
+
+class Redis():
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.host = host
+        self.port = port
+        self.db = db
