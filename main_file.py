927766d6271c542c6f83e38b450f0232a812aea2
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-02-16 22:34:14 +0300

    Hided Token

--- main_file.py
+++ main_file.py
@@ -14,7 +14,7 @@ from utils import TestStates
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token='1428472204:AAHSyI8mb6DBLAdX7o5if3XxTjvNfntN8ko')
+bot = Bot(token='TOKEN')
 
 dp = Dispatcher(bot, storage=RedisStorage('localhost', 6379, db=5))
 dp.middleware.setup(LoggingMiddleware())
