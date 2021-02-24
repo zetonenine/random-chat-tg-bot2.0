03ba1713f841f6e001544a4a2c2635d88fdf7364
Andrey Zaytsev <66561503+zetonenine@users.noreply.github.com>
2021-02-24 14:17:03 +0300

    Update postgresqler.py

--- postgresqler.py
+++ postgresqler.py
@@ -3,13 +3,28 @@ import psycopg2
 
 class BD:
 
-    def __init__(self, user="postgres", password="postgres", dbname="users", host="localhost"):
+    def __init__(self, user="dvasvcayevscby", password="e8b87679d0dbfeabde476ba8fef3f904aa5e1f5aeb6dcb1dd0a7dc096005eece", dbname="dadjq0evhseks", host="ec2-52-209-134-160.eu-west-1.compute.amazonaws.com
+"):
         self.connection = psycopg2.connect(
             user=user,
             password=password,
             dbname=dbname,
             host=host)
         self.cursor = self.connection.cursor()
+    
+    def create(self):
+        command = (
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                chat INTEGER NOT NULL,
+                partner_chat INTEGER,
+                status BOOLEAN DEFAULT FALSE
+                )
+            """
+        )    
+        with self.connection:
+            return self.cursor.execute(command)
 
     def add_user(self, chatID, status=False):
         with self.connection:
