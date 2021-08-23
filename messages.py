49213eb3150c369abc45cee2921e36fc426b25be
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-08-23 22:17:02 +0300

    message.py большое обновление

--- messages.py
+++ messages.py
@@ -73,10 +73,39 @@ match_1st_message = ('Match! Your partner is <b>{0}</b>\nWho will speak first? :
 
 match_2nd_message = 'Match! Your partner is <b>{0}</b>\nWho will speak first? :)'.format(random.choice(NAMES))
 
+no_partner_message = 'Unfortunately, all users are already busy. Try again'
+
+sure_stop_chat = 'Are you sure you want to end the conversation? Write <b>yep</b> to the chat'
+
 stop_1st_message = 'You had stopped the conversation..\nDo you want to /find new partner?'
 
 stop_2nd_message = 'Partner has stopped the conversation..\nDo you want to /find new partner?'
 
+editor_mode_main = 'Enter login and password:'
+
+editor_menu = 'You entered editor mode. What are you going to do?\n/show_banners\n/add_new_banner\n/del_banner\n/quit'
+
+log_in_success = 'Welcome home!'
+
+log_in_unsuccess = "Wrong login or password"
+
+stop_editor_mode = 'You logged out from editor mode'
+
+empty_banners = "There is no commercials"
+
+send_new_banner = "Send new commercial text"
+
+banner_was_added = "New commercial was added successfully"
+
+banner_was_not_added = "New commercial wasn't added"
+
+choose_banner_to_del = 'Write banners id to remove'
+
+error_id = "An error occurred when specifying an identifier. Try again: write just ID"
+
+banner_was_delete = "Commercial was removed successfully. ID "
+
+banner_was_not_delete = "No commercial with ID "
 
 MESSAGES = {
     "start": start_message,
@@ -86,9 +115,25 @@ MESSAGES = {
     'get': get_phrases_message,
     'match_1': match_1st_message,
     'match_2': match_2nd_message,
+    'no_partner_message': no_partner_message,
     'stop_1': stop_1st_message,
     'stop_2': stop_2nd_message,
+    'sure_stop_chat': sure_stop_chat,
+    'editor_mode': editor_mode_main,
+    'editor_menu': editor_menu,
+    'log_in_success': log_in_success,
+    'log_in_unsuccess': log_in_unsuccess,
+    'empty_banners': empty_banners,
+    'stop_editor_mode': stop_editor_mode,
+    'choose_banner_to_del': choose_banner_to_del,
+    'error_id': error_id,
+    'banner_was_added': banner_was_added,
+    'banner_was_not_added': banner_was_not_added,
+    'banner_was_delete': banner_was_delete,
+    'banner_was_not_delete': banner_was_not_delete,
+    'send_new_banner': send_new_banner,
 
 }
+
 
 
