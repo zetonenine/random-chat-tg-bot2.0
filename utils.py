2c02b1a6f3af7bff0a2868f0fd78b2e78ded4c9c
Andrey <66561503+zetonenine@users.noreply.github.com>
2021-08-23 22:18:41 +0300

    utils.py, requirements.py большое обновление

--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-class TestStates(StatesGroup):
-
+class BotStates(StatesGroup):
     chat_process = State()
+    stop_chat_process = State()
+    login_process = State()
+    editor_mode = State()
+    add_banner_process = State()
+    del_banner_process = State()
 
 
 if __name__ == '__main__':
-    print(TestStates.all())
+    print(BotStates.all())
